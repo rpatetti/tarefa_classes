f21eb5388f10329a2a3fc0f8d6a4e005dd87f08c
rpatetti <patetti.rafael@gmail.com>
2024-10-02 11:06:13 -0300

    initial commit

---
+++ src/exercicio_modulo_7/Carro.java
@@ -0,0 +1,38 @@
+package exercicio_modulo_7;
+
+public class Carro {
+
+	private String marca;
+	
+	private String modelo;
+	
+	//entrar com ano de modelo nao de fabricacao
+	
+	private int ano;
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+
+}
